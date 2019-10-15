c3bf3f574c748fc566382d3831d7cacaea846085
Victor <victorfreitas26@gmail.com>
2019-10-15 20:39:08 -0300

    classes adicionais

---
+++ ProjetoDevWeb/src/java/modelos/Gerente.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Gerente {
+    private int numeroFuncionario;
+
+    public int getNumeroFuncionario() {
+        return numeroFuncionario;
+    }
+
+    public void setNumeroFuncionario(int numeroFuncionario) {
+        this.numeroFuncionario = numeroFuncionario;
+    }
+    
+    
+}
